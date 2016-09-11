95578e6b597190dd3ee666a0021a07c5b6bdb5fc
edot92 <edot92@gmail.com>
2016-09-12 02:40:05 +0700

    add link palsu

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -45,7 +45,7 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-      header("Location: http://nitipsempak.com/");
+      header("Location: http://www.1227.com/");
       die();
 
       //  return parent::render($request, $exception);
