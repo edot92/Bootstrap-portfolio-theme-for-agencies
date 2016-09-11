ad608b60ba20d323586c9c69eab5b0bfbc5ae199
edot92 <edot92@gmail.com>
2016-09-12 04:05:58 +0700

    add repair config  nalaytic4

--- app/Modules/Home/Resources/Views/template/js_end_body.blade.php
+++ app/Modules/Home/Resources/Views/template/js_end_body.blade.php
@@ -1,6 +1,6 @@
 <script type="text/javascript">
   $(document).ready(function() {
     var a="<?php if(isset($varTemp)){echo ($varTemp);} else{echo "no var";} ?>";
-    alert(a);
+    // alert(a);
   });
 </script>
