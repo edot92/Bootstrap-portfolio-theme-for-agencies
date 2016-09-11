6cf2f4a09a114f4beabf3023f48f5f2c7bae967d
edot92 <edot92@gmail.com>
2016-09-12 04:19:27 +0700

    add repair config  nalaytic6

--- app/Modules/Home/Resources/Views/template/head.blade.php
+++ app/Modules/Home/Resources/Views/template/head.blade.php
@@ -4,8 +4,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="google-site-verification" content="@yield('deskripsi_seo')" />
-    <meta name="author" content="">
-
+    <meta name="author" content="penjual mobil">
+    <link rel="alternate" href="http://sewamobildibali.96.lt" hreflang="id-id" />
      <title>@yield('title')</title>
 
     <!-- Bootstrap Core CSS -->
