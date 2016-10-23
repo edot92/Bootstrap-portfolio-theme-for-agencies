864bbb22d48b72399c2f1fda07a5de47284f6edb
edot92 <edot92@gmail.com>
2016-10-23 19:15:45 +0700

    add peta

--- app/Modules/Home/Resources/Views/template/footer.blade.php
+++ app/Modules/Home/Resources/Views/template/footer.blade.php
@@ -9,15 +9,11 @@
 			Lorem ipsum dolor sit amet, consectateur adispicing elit. Fusce euismod convallis velit, eu auctor lacus vehicula sit amet.
 		</p> --}}
 
-				<div>
-					<i class="fa fa-map-marker"></i>
-					<p><span>Jalan Kubu Anyar no.12 Kuta Badung </span> Bali, INDONESIA</p>
+
+				<div class="embed-responsive embed-responsive-4by3">
+						<iframe class="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1971.7733881423862!2d115.16767675782866!3d-8.734512798431368!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd2441e28e8628b%3A0x88e9b313a32edd59!2sJl.+Kubu+Anyar+No.12%2C+Kuta%2C+Kabupaten+Badung%2C+Bali+80361!5e0!3m2!1sid!2sid!4v1477224413927" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 				</div>
 
-				<div>
-					<i class="fa fa-envelope"></i>
-					<p><a href="mailto:support@company.com">mertaretcar@gmail.com</a></p>
-				</div>
 	</div>
 
 
@@ -39,10 +35,24 @@
 
 	</div>
 
-	<div class="footer-right">
+	<div class="footer-right" style="color:white">
 		{{-- <p class="footer-links">
 		</p> --}}
+		<div class="fa fa-home">
+			<i>
+						<p class="footer-company-name">PT. MERTA DANA RENTCAR &copy; 2016</p>
+			</i>
+		</div>
 
-		<p class="footer-company-name">PT. MERTA DANA RENTCAR &copy; 2016</p>
+				<div>
+					<i class="fa fa-map-marker"></i>
+					<p><span>Jalan Kubu Anyar no.12 Kuta Badung </span> Bali, INDONESIA</p>
+				</div>
+		<div>
+			<i class="fa fa-envelope"></i>
+			<p><a href="mailto:support@company.com">mertaretcar@gmail.com</a></p>
+		</div>
+
+
 	</div>
 </footer>
